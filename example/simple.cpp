b4d87b29e908a981bdcd41e8161010dd795be603
Johann Muszynski <johann.muszynski@gmail.com>
2018-10-20 10:59:32 +0300

    Improved simple example

--- example/simple.cpp
+++ example/simple.cpp
@@ -1,19 +1,26 @@
 #include "node_editor.h"
 #include "imnodes.h"
 
+#include <unordered_map>
 #include <vector>
 
 namespace example
 {
 namespace
 {
+struct Color3
+{
+    float data[3];
+};
+
 inline int make_id(int node, int attribute) { return (node << 16) | attribute; }
 
 class SimpleNodeEditor
 {
 public:
     SimpleNodeEditor()
-        : current_id_(0), red_nodes_(), green_nodes_(), blue_nodes_()
+        : current_id_(0), input_nodes_(), output_nodes_(), dragfloat_nodes_(),
+          big_nodes_(), dragfloat_data_(), color_data_()
     {
     }
 
@@ -23,118 +30,161 @@ public:
         imnodes::BeginNodeEditor();
 
         imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBar, IM_COL32(255, 30, 30, 255));
+            imnodes::ColorStyle_TitleBar, IM_COL32(170, 88, 57, 255));
         imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBarHovered, IM_COL32(255, 80, 80, 255));
-        for (int red : red_nodes_)
+            imnodes::ColorStyle_TitleBarHovered, IM_COL32(212, 135, 106, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarSelected, IM_COL32(255, 193, 170, 255));
+        for (int input_node : input_nodes_)
+        {
+            imnodes::BeginNode(input_node);
+            imnodes::Name("Input");
+            imnodes::BeginAttribute(
+                make_id(input_node, 0), imnodes::AttributeType_Input);
+            ImGui::Text("input");
+            imnodes::EndAttribute();
+            imnodes::EndNode();
+        }
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarSelected);
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarHovered);
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBar);
+
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBar, IM_COL32(170, 121, 57, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarHovered, IM_COL32(212, 166, 106, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarSelected, IM_COL32(255, 218, 170, 255));
+        for (int output_node : output_nodes_)
+        {
+            imnodes::BeginNode(output_node);
+            imnodes::Name("Output");
+            imnodes::BeginAttribute(
+                make_id(output_node, 0), imnodes::AttributeType_Output);
+            ImGui::Text("output");
+            imnodes::EndAttribute();
+            imnodes::EndNode();
+        }
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarSelected);
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarHovered);
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBar);
+
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBar, IM_COL32(41, 81, 109, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarHovered, IM_COL32(72, 109, 136, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarSelected, IM_COL32(112, 142, 164, 255));
+        for (int dragfloat_node : dragfloat_nodes_)
         {
             const float node_width = 150.0f;
-            const float node_padding = 8.0f;
-            imnodes::BeginNode(red);
-            imnodes::Name("Red");
+            imnodes::BeginNode(dragfloat_node);
+            imnodes::Name("Drag float");
 
             imnodes::BeginAttribute(
-                make_id(red, 0), imnodes::AttributeType_Input);
-            ImGui::TextUnformatted("input");
+                make_id(dragfloat_node, 0), imnodes::AttributeType_Input);
+            ImGui::Text("input");
             imnodes::EndAttribute();
+            ImGui::Spacing();
 
-            ImGui::Dummy(ImVec2(node_width, 1.f));
+            {
+                // use an internal attribute when you don't want a pin to be
+                // drawn on the attribute
+                imnodes::BeginAttribute(
+                    make_id(dragfloat_node, 1),
+                    imnodes::AttributeType_Internal);
+                const float label_width = ImGui::CalcTextSize("number").x;
+                ImGui::Text("number");
+                ImGui::PushItemWidth(node_width - label_width - 6.0f);
+                ImGui::SameLine();
+                ImGui::DragFloat(
+                    "##hidelabel", &dragfloat_data_[dragfloat_node], 0.01f);
+                ImGui::PopItemWidth();
+                imnodes::EndAttribute();
+            }
 
-            imnodes::BeginAttribute(
-                make_id(red, 1), imnodes::AttributeType_Output);
-            const auto label_size = ImGui::CalcTextSize("output");
-            ImGui::Indent(node_width - label_size.x - node_padding);
-            ImGui::TextUnformatted("output");
-            imnodes::EndAttribute();
+            {
+                imnodes::BeginAttribute(
+                    make_id(dragfloat_node, 2), imnodes::AttributeType_Output);
+                const float label_width = ImGui::CalcTextSize("output").x;
+                ImGui::Indent(node_width - label_width - 1.5f);
+                ImGui::Text("output");
+                imnodes::EndAttribute();
+            }
 
             imnodes::EndNode();
         }
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarSelected);
         imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarHovered);
         imnodes::PopColorStyle(imnodes::ColorStyle_TitleBar);
 
         imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBar, IM_COL32(30, 255, 30, 255));
+            imnodes::ColorStyle_TitleBar, IM_COL32(39, 117, 82, 255));
         imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBarHovered, IM_COL32(100, 255, 100, 255));
-        for (int green : green_nodes_)
+            imnodes::ColorStyle_TitleBarHovered, IM_COL32(73, 147, 113, 255));
+        imnodes::PushColorStyle(
+            imnodes::ColorStyle_TitleBarSelected, IM_COL32(117, 176, 149, 255));
+        for (int big_node : big_nodes_)
         {
-            const float node_width = 80.0f;
-            const float node_padding = 8.0f;
-            imnodes::BeginNode(green);
-            imnodes::Name("Green");
+            const float node_width = 200.0f;
+            imnodes::BeginNode(big_node);
+            imnodes::Name("Big node");
 
             imnodes::BeginAttribute(
-                make_id(green, 0), imnodes::AttributeType_Input);
-            ImGui::TextUnformatted("input");
+                make_id(big_node, 0), imnodes::AttributeType_Input);
+            ImGui::Text("input1");
+            imnodes::EndAttribute();
+
+            ImGui::Spacing();
+
+            imnodes::BeginAttribute(
+                make_id(big_node, 1), imnodes::AttributeType_Input);
+            ImGui::Text("input2");
             imnodes::EndAttribute();
 
             {
                 imnodes::BeginAttribute(
-                    make_id(green, 1), imnodes::AttributeType_Internal);
+                    make_id(big_node, 2), imnodes::AttributeType_Output);
+                const float label_width = ImGui::CalcTextSize("number").x;
+                ImGui::PushItemWidth(node_width - label_width - 6.0f);
+                ImGui::DragFloat("number", &dragfloat_data_[big_node], 0.01f);
+                ImGui::PopItemWidth();
+                imnodes::EndAttribute();
+            }
+
+            ImGui::Spacing();
+
+            {
+                imnodes::BeginAttribute(
+                    make_id(big_node, 3), imnodes::AttributeType_Output);
                 const float label_width = ImGui::CalcTextSize("color").x;
-                ImGui::TextUnformatted("color");
-                ImGui::SameLine();
-                ImGui::PushItemWidth(node_width - label_width - node_padding);
-                // TODO: figure out where to store the color values
-                // ImGui::ColorEdit3("##hidelabel", );
+                ImGui::PushItemWidth(node_width - label_width - 6.0f);
+                ImGui::ColorEdit3("color", color_data_[big_node].data);
                 ImGui::PopItemWidth();
                 imnodes::EndAttribute();
             }
 
             {
                 imnodes::BeginAttribute(
-                    make_id(green, 1), imnodes::AttributeType_Output);
-                const auto label_size = ImGui::CalcTextSize("output").x;
-                ImGui::Indent(node_width - label_size - node_padding);
-                ImGui::TextUnformatted("output");
+                    make_id(big_node, 5), imnodes::AttributeType_Output);
+                const float label_width = ImGui::CalcTextSize("output1").x;
+                ImGui::Indent(node_width - label_width - 1.5f);
+                ImGui::Text("output1");
+                imnodes::EndAttribute();
+            }
+            ImGui::Spacing();
+            {
+                imnodes::BeginAttribute(
+                    make_id(big_node, 6), imnodes::AttributeType_Output);
+                const float label_width = ImGui::CalcTextSize("output2").x;
+                ImGui::Indent(node_width - label_width - 1.5f);
+                ImGui::Text("output2");
                 imnodes::EndAttribute();
             }
 
             imnodes::EndNode();
         }
-        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarHovered);
-        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBar);
-
-        imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBar, IM_COL32(30, 30, 255, 255));
-        imnodes::PushColorStyle(
-            imnodes::ColorStyle_TitleBarHovered, IM_COL32(80, 80, 255, 255));
-        for (int blue : blue_nodes_)
-        {
-            const float node_width = 100.0f;
-            const float node_padding = 8.0f;
-            imnodes::BeginNode(blue);
-            imnodes::Name("Blue");
-
-            imnodes::BeginAttribute(
-                make_id(blue, 0), imnodes::AttributeType_Input);
-            ImGui::TextUnformatted("asdf");
-            imnodes::EndAttribute();
-
-            {
-                imnodes::BeginAttribute(
-                    make_id(blue, 1), imnodes::AttributeType_Internal);
-                const float label_width = ImGui::CalcTextSize("vector").x;
-                ImGui::TextUnformatted("vector");
-                ImGui::SameLine();
-                ImGui::PushItemWidth(node_width - label_width - node_padding);
-                // TODO:
-                // ImGui::DragFloat3("##hidelabel");
-                ImGui::PopItemWidth();
-                imnodes::EndAttribute();
-            }
-
-            {
-                imnodes::BeginAttribute(
-                    make_id(blue, 2), imnodes::AttributeType_Output);
-                const float label_width = ImGui::CalcTextSize("asdfasdf").x;
-                ImGui::Indent(node_width - label_width);
-                ImGui::TextUnformatted("asdfasdf");
-                imnodes::EndAttribute();
-            }
-
-            imnodes::EndNode();
-        }
+        imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarSelected);
         imnodes::PopColorStyle(imnodes::ColorStyle_TitleBarHovered);
         imnodes::PopColorStyle(imnodes::ColorStyle_TitleBar);
 
@@ -153,22 +203,31 @@ public:
             int new_node = -1;
             ImVec2 click_pos = ImGui::GetMousePosOnOpeningCurrentPopup();
 
-            if (ImGui::MenuItem("Red"))
+            if (ImGui::MenuItem("Input only"))
             {
                 new_node = current_id_++;
-                red_nodes_.push_back(new_node);
+                input_nodes_.push_back(new_node);
             }
 
-            if (ImGui::MenuItem("Blue"))
+            if (ImGui::MenuItem("Output only"))
             {
                 new_node = current_id_++;
-                blue_nodes_.push_back(new_node);
+                output_nodes_.push_back(new_node);
             }
 
-            if (ImGui::MenuItem("Green"))
+            if (ImGui::MenuItem("Drag float"))
             {
                 new_node = current_id_++;
-                green_nodes_.push_back(new_node);
+                dragfloat_nodes_.push_back(new_node);
+                dragfloat_data_[new_node] = 1.0f;
+            }
+
+            if (ImGui::MenuItem("Big node"))
+            {
+                new_node = current_id_++;
+                dragfloat_data_[new_node] = 0.0f;
+                color_data_[new_node] = Color3{1.0f, 0.0f, 0.0f};
+                big_nodes_.push_back(new_node);
             }
 
             ImGui::EndPopup();
@@ -186,9 +245,14 @@ public:
 
 private:
     int current_id_;
-    std::vector<int> red_nodes_;
-    std::vector<int> green_nodes_;
-    std::vector<int> blue_nodes_;
+
+    std::vector<int> input_nodes_;
+    std::vector<int> output_nodes_;
+    std::vector<int> dragfloat_nodes_;
+    std::vector<int> big_nodes_;
+
+    std::unordered_map<int, float> dragfloat_data_;
+    std::unordered_map<int, Color3> color_data_;
 };
 
 static SimpleNodeEditor editor;
