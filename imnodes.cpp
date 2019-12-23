b73fd0760ace3c057fa24a7517815fcab5e49581
Johann Muszynski <johann.muszynski@gmail.com>
2019-12-23 12:47:12 +0200

    Add check for minimum compatible ImGui version

--- imnodes.cpp
+++ imnodes.cpp
@@ -13,6 +13,13 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui_internal.h"
 
+// Check minimum ImGui version
+#define MINIMUM_COMPATIBLE_IMGUI_VERSION 16401
+#if IMGUI_VERSION_NUM < MINIMUM_COMPATIBLE_IMGUI_VERSION
+#error                                                                         \
+    "Minimum ImGui version requirement not met -- please use a newer version!"
+#endif
+
 #include <assert.h>
 #include <new>
 #include <stdint.h>
