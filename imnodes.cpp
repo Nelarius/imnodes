7d2228926fea99cd19c8f6ad52f9455838c1ff68
Johann Muszynski <johann.muszynski@gmail.com>
2020-05-15 09:25:10 +0300

    Update to latest imgui version

--- imnodes.cpp
+++ imnodes.cpp
@@ -9,9 +9,9 @@
 
 #include "imnodes.h"
 
-#include "imgui.h"
+#include <imgui.h>
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui_internal.h"
+#include <imgui_internal.h>
 
 // Check minimum ImGui version
 #define MINIMUM_COMPATIBLE_IMGUI_VERSION 16401
