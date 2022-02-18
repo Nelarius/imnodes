e5b5a147aa5c777b53c71b8e2e4d8a5ede2b4543
dianwoshishi <dianwoshishi@sina.com>
2022-02-18 01:58:04 -0800

    Linux cmake: fix the compile errors of link_library order on ubuntu.

--- cmake/modules/FindPcapPlusPlus.cmake
+++ cmake/modules/FindPcapPlusPlus.cmake
@@ -1,9 +1,14 @@
-FIND_PATH(PcapPlusPlus_INCLUDE_DIR pcapplusplus/Layer.h)
-# message("[+] pcapplusplus include dir is : ${pcapplusplus_include_dir}")
-FIND_PATH(PcapPlusPlus_DIR NAMES lib/libCommon++.a lib/libPacket++.a)
+FIND_PATH(PcapPlusPlus_INCLUDE_DIR 
+    NAMES pcapplusplus/Layer.h 
+    PATHS  /usr/local/)
+message("[+] pcapplusplus include dir is : ${PcapPlusPlus_INCLUDE_DIR}")
+FIND_PATH(PcapPlusPlus_DIR 
+    NAMES lib/libCommon++.a lib/libPacket++.a  lib/libPcap++
+    PATHS /usr/local/)
+message("[+] pcapplusplus include dir is : ${PcapPlusPlus_DIR}")
 
-IF( PcapPlusPlus_INCLUDE_DIR AND pcapplusplus_dir)
+IF( PcapPlusPlus_INCLUDE_DIR AND PcapPlusPlus_DIR)
     set(PcapPlusPlus_FOUND TRUE)
-    set(PcapPlusPlus_LIBS_DIR ${pcapplusplus_dir}/lib)
+    set(PcapPlusPlus_LIBS_DIR ${PcapPlusPlus_DIR}/lib)
     # message("[+] pcapplusplus libs dir is : ${PcapPlusPlus_LIBS_DIR}")
 ENDIF()
