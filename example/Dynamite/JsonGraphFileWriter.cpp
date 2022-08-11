e28dbd8b88fee8f77678327c4b93e0944c158528
Jehan Diaz <jehan.diaz@sonos.com>
2022-08-11 14:27:03 -0400

    traverse _block in order

--- example/Dynamite/JsonGraphFileWriter.cpp
+++ example/Dynamite/JsonGraphFileWriter.cpp
@@ -1,13 +1,13 @@
 #include "JsonGraphFileWriter.h"
 
+/*
+ERRORS: 
+- finding it difficult to change block input/output names and adding new ones -> clicking on the mpp boxes is weird
+- must use string literal with AddmMember(), can't assign specific block values to value arg
+*/
+
 void JsonGraphFileWriter::writeToFile(Context& context) {
     // set up .json file
-    /* method 1
-    ofstream ofs("system.json");    // create output file
-    OStreamWrapper osw(ofs);        // stream wrapper
-    Writer<OStreamWrapper> writer(osw); // writter for the stream wrapper
-    */
-    // method 2
     FILE* fp = fopen("system.json", "w");
     char buffer[65536];
     FileWriteStream fs(fp, buffer, sizeof(buffer));
@@ -32,52 +32,49 @@ void JsonGraphFileWriter::writeToFile(Context& context) {
 
     Value dsp_blocks(kArrayType); // AddMember("dsp_blocks", dsp_blocks, jsonDoc.GetAllocator()) is the last line
     Value block;
-    block.SetObject();
 
-    string name = "input";
-    auto iter = std::find_if(
-    context._blocks.begin(), context._blocks.end(), [name](Block& block) -> bool {
-        return block.getName() == name;
-    });
-    block.AddMember("name", "input", allocator);
+    for (Block& b: context._blocks) {
+        block.SetObject();
+        block.AddMember("name", "block name", allocator);
 
-    Value input_chans(kArrayType);
-    map<int, Port>::iterator it;
-    for (it = iter->_inPorts.begin(); it != iter->_inPorts.end(); it++) {
-        Value input_ch;
-        input_ch.SetObject();
-        //input_ch.AddMember("name", it->second.c_str(), allocator);
-        input_ch.AddMember("name", "input", allocator);
-        input_chans.PushBack(input_ch, allocator);
+        Value input_chans(kArrayType);
+        map<int, Port>::iterator it;
+        for (it = b._inPorts.begin(); it != b._inPorts.end(); it++) {
+            Value input_ch;
+            input_ch.SetObject();
+            /*
+            string s = it->second.type;
+            Value type(s.c_str(), s.size(), allocator);
+            input_ch.AddMember("name", s, allocator); */
+            input_ch.AddMember("name", "input", allocator);
+            input_chans.PushBack(input_ch, allocator);
+        }
+        block.AddMember("input_channels", input_chans, allocator);
+
+        Value output_chans(kArrayType);
+        for (it = b._outPorts.begin(); it != b._outPorts.end(); it++) {
+            Value output_ch;
+            output_ch.SetObject();
+            //output_ch.AddMember("name", it->second.c_str(), allocator);
+            output_ch.AddMember("name", "output", allocator);
+            output_chans.PushBack(output_ch, allocator);
+        }
+        block.AddMember("output_channels", output_chans, allocator);
+
+        /*
+        Value param;
+        param.SetObject();
+        map<int,Parameter>::iterator itp;
+        for (itp = iter->_parameters.begin(); itp != iter->_parameters.end(); itp++) {
+            param.AddMember(iter->second.name, iter->second.value, allocator);
+        }
+        block.AddMember(iter->getName, param, allocator);
+        */
+
+       dsp_blocks.PushBack(block, allocator);
     }
-    block.AddMember("input_channels", input_chans, allocator);
-
-    Value output_chans(kArrayType);
-    for (it = iter->_outPorts.begin(); it != iter->_outPorts.end(); it++) {
-        Value output_ch;
-        output_ch.SetObject();
-        //output_ch.AddMember("name", it->second.c_str(), allocator);
-        output_ch.AddMember("name", "output", allocator);
-        output_chans.PushBack(output_ch, allocator);
-    }
-    block.AddMember("output_channels", output_chans, allocator);
-
-    /*
-    Value param;
-    param.SetObject();
-    map<int,Parameter>::iterator itp;
-    for (itp = iter->_parameters.begin(); itp != iter->_parameters.end(); itp++) {
-        param.AddMember(iter->second.name, iter->second.value, allocator);
-    }
-    block.AddMember(iter->getName, param, allocator);
-    */
-    dsp_blocks.PushBack(block, allocator);
-    /*
-    iterate over blocks
-    add each block to t
-    block.SetObject();
-    */
     jsonDoc.AddMember("dsp_blocks", dsp_blocks, allocator);
+
     // write to file
     jsonDoc.Accept(pwriter);
 }
