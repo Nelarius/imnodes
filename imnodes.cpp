693995088a17d0b9a207be1c913d5f86bf321ffb
Jordan Peck <jordan.me2@gmail.com>
2024-02-19 22:14:51 +0000

    Fix compile with latest ImGui (#181)
    IMGUI_DEFINE_MATH_OPERATORS now needs to be defined before #include <imgui.h>

--- imnodes.cpp
+++ imnodes.cpp
@@ -6,11 +6,7 @@
 // [SECTION] render helpers
 // [SECTION] API implementation
 
-#include "imnodes.h"
 #include "imnodes_internal.h"
-
-#define IMGUI_DEFINE_MATH_OPERATORS
-#include <imgui_internal.h>
 
 // Check minimum ImGui version
 #define MINIMUM_COMPATIBLE_IMGUI_VERSION 17400
